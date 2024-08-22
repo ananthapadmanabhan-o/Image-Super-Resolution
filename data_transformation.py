1650c8df6cd6883784caab54ce0f5da60b50f9d7
ananthapadmanabhan-o <ananthan51ah@gmail.com>
2024-08-22 19:04:33 +0530

    modified data transformation

--- data_transformation.py
+++ data_transformation.py
@@ -1,23 +1,23 @@
 from srgan.data import DataTransformation
-from srgan.utils import read_yaml, create_directories
+from srgan.utils import read_yaml
 
 def main():
         
     config = read_yaml('config.yaml')
-    data_transformation_config = config.data_transformation
+    data_config = config.DATA_TRANSFORMATION
 
 
     train_data_transformation = DataTransformation(
-        source_dir=data_transformation_config.train_source_dir,
-        destination_dir=data_transformation_config.train_data_transformed
+        source_dir=data_config.TRAIN_SOURCE_DIR,
+        destination_dir=data_config.TRAIN_DATA_TRANSFORMED
     )
 
     train_data_transformation.transform_data()
 
 
     valid_data_transformation = DataTransformation(
-        source_dir=data_transformation_config.valid_source_dir,
-        destination_dir=data_transformation_config.valid_data_transformed
+        source_dir=data_config.VALID_SOURCE_DIR,
+        destination_dir=data_config.VALID_DATA_TANSFORMED
     )
 
     valid_data_transformation.transform_data()
