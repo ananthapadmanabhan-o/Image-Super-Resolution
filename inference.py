05adfe0a7a84e72414456fdf5ebc2dd433684188
ananthapadmanabhan-o <ananthan51ah@gmail.com>
2024-08-23 16:18:37 +0530

    inferece modified

--- inference.py
+++ inference.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import torch
 from torchvision.transforms.functional import pil_to_tensor, to_pil_image
+import warnings
+warnings.filterwarnings('ignore')
 
 
 
@@ -8,18 +10,20 @@ from torchvision.transforms.functional import pil_to_tensor, to_pil_image
 img_path = '13_hr.png'
 model_path = 'models/model.pth'
 output_path = 'runs/output.png'
+device = 'cuda'
 
 
 
 
 img = Image.open(img_path)
-img_tensor = pil_to_tensor(img)
-input_tensor = torch.unsqueeze(img_tensor,0)
+img_tensor = pil_to_tensor(img)/255
+input_tensor = torch.unsqueeze(img_tensor,0).to(device)
 
-model = torch.load(model_path)
+model = torch.load(model_path).to(device)
+model.eval()
 print(f'Model loaded from {model_path}')
 
-with torch.inference_mode():
+with torch.no_grad():
     output_tensor = model(input_tensor)
 
 out_img = to_pil_image(torch.squeeze(output_tensor,0))
