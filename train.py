fa9b5775a02b6d0682e95528e5fcc4dd1d444f84
ananthapadmanabhan-o <ananthan51ah@gmail.com>
2024-08-23 18:35:27 +0530

    train file modified

--- train.py
+++ train.py
@@ -2,7 +2,6 @@ from srgan.data import SrganDataset
 from srgan.model import Generator, Discriminator
 from srgan.losses import GenLoss, DiscLoss
 from torch.optim import Adam 
-from torchvision import transforms
 from srgan.utils import read_yaml, create_directories
 from srgan.train import SrganTrainer
 
@@ -11,31 +10,25 @@ from srgan.train import SrganTrainer
 def main():
     config = read_yaml('config.yaml')
 
-    
     model_config = config.MODEL
-    data_config = config.DATA_TRANSFORMATION
+    data_config = config.DATA
 
 
     create_directories([model_config.ROOT_DIR])
 
-    
-    
-    data_transforms = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.4112, 0.4435, 0.4562],std=[0.1613, 0.1644, 0.1737]),
-    ])
-
 
     dataset = SrganDataset(
-    root_dir=data_config.VALID_DATA_TANSFORMED,
-    downscale=int(data_config.DOWN_SCALE),
-    transform=data_transforms
+    root_dir=data_config.TRAIN_SOURCE_DIR,
+    crop_size=data_config.CROP_SIZE,
+    downscale=int(data_config.DOWN_SCALE)
     )
 
     gen = Generator(num_blocks=int(model_config.NUM_BLOCKS),scale_factor=int(model_config.SCALE_FACTOR))
     disc = Discriminator()
+
     g_loss = GenLoss()
     d_loss = DiscLoss()
+    
     g_optim = Adam(gen.parameters(),lr = float(model_config.LR))
     d_optim = Adam(disc.parameters(),lr = float(model_config.LR))
 
